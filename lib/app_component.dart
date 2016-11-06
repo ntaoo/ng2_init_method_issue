0a0c8af945d727df80999e70c7d65f83fe89464a
ntaoo <ntaoo.g@gmail.com>
2016-11-06 17:56:31 +0900

    first commit

---
+++ lib/app_component.dart
@@ -0,0 +1,15 @@
+// Copyright (c) 2016, ntaoo. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:angular2/core.dart';
+import 'model.dart';
+
+@Component(
+    selector: 'my-app',
+    styleUrls: const ['app_component.css'],
+    templateUrl: 'app_component.html')
+class AppComponent {
+  AppComponent() {
+    init();
+  }
+}
